804015376453aac1d55ad9387b44846ba9960338
meso5533 <463785757@qq.com>
2015-11-13 17:46:18 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,7 @@ git clone git@github.com:vim-scripts/taglist.vim.git
 git clone git@github.com:fatih/vim-go.git #需在vim中运行 :GoInstallBinaries
 git clone git@github.com:Yggdroot/indentLine.git
 git clone git@github.com:ianva/vim-youdao-translater.git
+git clone git@github.com:klen/python-mode.git
 git clone git@github.com:Valloric/YouCompleteMe.git && \
     cd ~/.vim/bundle/YouCompleteMe && \
         git submodule update --recursive && \
