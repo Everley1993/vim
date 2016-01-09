801d089151d2b605996c5eea0e8b999b9fe4e237
meso5533 <463785757@qq.com>
2016-01-09 13:28:11 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ sudo add-apt-repository ppa:numix/ppa
 sudo apt-get update
 sudo apt-get install numix-gtk-theme numix-icon-theme numix-icon-theme-circle numix-wallpaper-saucy
 sudo apt-get install unity-tweak-tool
+sudo apt-get install plank
 sudo apt-get install guake
 sudo apt-get install vim
 sudo apt-get install git
